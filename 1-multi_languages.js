32dada10837611f8dfbd4cdf0fe8bdcca6824e75
Privilege Mupindu <privilegemupindu@gmail.com>
2025-06-01 20:31:04 +0300

    A script that prints 3 lines

---
+++ 1-multi_languages.js
@@ -0,0 +1,8 @@
+//1-multi_language.js
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
